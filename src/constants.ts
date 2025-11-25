a88974652d02b5285de8fd658ab9ff1afed03421
nichoth <nichoth@nichoth.com>
2025-11-25 10:38:39 -0800

    add everything, setup

---
+++ src/constants.ts
@@ -0,0 +1,8 @@
+export const TAG = 'skyprism'
+export const WORKER_ENDPOINT = 'https://atprism-feed-worker.nichoth.workers.dev'
+export const FIREHOSE_URL = 'wss://bsky.network'
+export const BSKY_DID = 'did:web:atprism-feed-worker.nichoth.workers.dev'
+export const BSKY_HANDLE='atprism.bsky.social'
+export const FEEDGEN_HOSTNAME = 'atprism-feed-worker.nichoth.workers.dev'
+export const RECORD_NAME = 'skyprism'
+export const PUBLIC_KEY_MULTIBASE = "zQ3shrnbmRBq9tFvirWPhvWWSduHEQcFxKZYcGybRDou4jgx9"
